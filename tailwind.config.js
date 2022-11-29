483cc0e5c308e004487cee7e8da8ddd799e70f48
dl2574 <20144415+dl2574@users.noreply.github.com>
2022-11-29 14:31:43 +0900

    initial setup - copy from other start - tailwind, dotenv, template and static files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#03045E',
+      'secondary': '#00B4D8',
+      'third': '#90E0EF',
+      'fourth': '#CAF0F8'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
