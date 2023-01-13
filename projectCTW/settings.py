b4d8e55639e11a3b31f201e46a17ad3455ca3d70
dl2574 <20144415+dl2574@users.noreply.github.com>
2023-01-14 06:51:24 +0900

    added local host and port env vars to ci.yml

--- projectCTW/settings.py
+++ projectCTW/settings.py
@@ -96,23 +96,23 @@ DATABASES = {
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
 
-    # 'default': {
-    #     'ENGINE': "django.db.backends.postgresql",
-    #     'NAME': (os.getenv("PGDATABASE") if os.getenv("PGDATABASE") else "projectCTW"),
-    #     'USER': os.getenv("PGUSER"),
-    #     'PASSWORD': os.getenv("PGPASSWORD"),
-    #     'HOST': os.getenv("PGHOST"),
-    #     'PORT': os.getenv("PGPORT"),
-    # }
-
     'default': {
         'ENGINE': "django.db.backends.postgresql",
-        'NAME': "postgres",
-        'USER': "postgres",
-        'PASSWORD': "postgres",
-        'HOST': "db",
-        'PORT': "5432",
+        'NAME': (os.getenv("PGDATABASE") if os.getenv("PGDATABASE") else "projectCTW"),
+        'USER': os.getenv("PGUSER"),
+        'PASSWORD': os.getenv("PGPASSWORD"),
+        'HOST': os.getenv("PGHOST"),
+        'PORT': os.getenv("PGPORT"),
     }
+
+    # 'default': {
+    #     'ENGINE': "django.db.backends.postgresql",
+    #     'NAME': "postgres",
+    #     'USER': "postgres",
+    #     'PASSWORD': "postgres",
+    #     'HOST': "db",
+    #     'PORT': "5432",
+    # }
 }
 
 
