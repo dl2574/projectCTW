571c04024bca12f0550a579b309f429867b62449
dl2574 <20144415+dl2574@users.noreply.github.com>
2023-11-21 17:11:10 -0600

    Add Whitenoise

--- projectCTW/settings.py
+++ projectCTW/settings.py
@@ -149,6 +149,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 
