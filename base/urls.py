9565245ce57672858dd7379ae2b61e5ee3bc163c
dl2574 <20144415+dl2574@users.noreply.github.com>
2023-11-23 17:17:00 -0600

    Display site construction message

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("message-test/", views.message_test, name="message_test"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def home(request):
+    messages.info(request, "Please excuse our mess. This site is still under construction.")
     return render(request, "base/home.html")
 
 
 def about(request):
     return render(request, "base/about.html")
+
+
+def message_test(request):
+    messages.info(request, "Test Info")
+    messages.success(request, "Test Success")
+    messages.warning(request, "Test Warning")
+    messages.error(request, "Test Error")
+    return render(request, "base/messages.html")
