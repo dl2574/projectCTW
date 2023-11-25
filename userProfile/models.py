750b8943646749e1f6f539cef33d6ffb1aee031a
dl2574 <20144415+dl2574@users.noreply.github.com>
2023-11-24 23:54:59 -0500

    updated user ID field

--- userProfile/models.py
+++ userProfile/models.py
@@ -1,10 +1,14 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
