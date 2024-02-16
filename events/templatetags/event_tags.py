f617a22a6c39175ef2941d2cd9bcf72ba335eea8
dl2574 <20144415+dl2574@users.noreply.github.com>
2024-02-16 21:26:33 +0900

    Add color, background, and boarder to status tags

--- events/templatetags/event_tags.py
+++ events/templatetags/event_tags.py
@@ -17,18 +17,20 @@ def upvoted(event, user):
 # register.filter("upvoted", upvoted)
 
 @register.simple_tag
-def event_status(status):
-    color = "yellow"
+def event_status_color(status):
+    color = "text-yellow-700 border-yellow-500 bg-yellow-100"
     if status == "PL":
-        color = "purple"
+        color = "text-purple-700 border-purple-500 bg-purple-100"
     elif status == "SC":
-        color = "blue"
+        color = "text-blue-700 border-blue-500 bg-blue-100"
     elif status == "CO":
-        color = "green"
+        color = "text-green-700 border-green-500 bg-green-100"
     elif status == "AR":
-        color = "gray"
+        color = "text-gray-700 border-gray-500 bg-gray-100"
     elif status == "DN":
-        color = "red"
+        color = "text-red-700 border-red-500 bg-red-100"
     elif status == "RM":
-        color = "red"
-    return f"text-{color}-400"
+        color = "text-red-700 border-red-500 bg-red-100"
+        
+    #return f'<span class="{color} px-3 py-1 rounded-xl border-2">{ event.get_status_display }</span>'
+    return color
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js,py}"],
+  safelist: [
+    ''
+  ],
   theme: {
     extend: {},
   },
