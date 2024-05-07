8343090fa60d7df161935d94f417f3a79c4acd2b
dl2574 <20144415+dl2574@users.noreply.github.com>
2024-05-06 23:17:49 -0700

    Add date age function to User profile and started commenting in additional fields to think about

--- userProfile/models.py
+++ userProfile/models.py
@@ -1,14 +1,21 @@
 import uuid
+
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import SET_NULL
 
+from datetime import date
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
+    # experience = 
+    # phone number = 
+    # following = models.ForeignKey("User", on_delete=SET_NULL, null=True)
+    # followers = 
     
 
     USERNAME_FIELD = 'email'
@@ -22,4 +29,8 @@ class User(AbstractUser):
     
     def get_absolute_url(self):
         return reverse("account_profile", kwargs={"slug": self.username})
+    
+    def get_age(self):
+        today = date.today()
+        return today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
     
