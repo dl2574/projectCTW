30cf8277bb752e06ecdc319ef47a6dffcba9c6cd
dl2574 <20144415+dl2574@users.noreply.github.com>
2024-05-17 22:46:45 -0700

    Modified notification code to fix bugs

--- notifications/models.py
+++ notifications/models.py
@@ -2,13 +2,10 @@ from django.db import models
 
 from django.contrib.auth import get_user_model
 
-from ..events.models import Event
-
 User = get_user_model()
 
 class Notification(models.Model):
     message = models.CharField(max_length=150, blank=True)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_created=True)
     read = models.BooleanField(default=False)
     
@@ -20,8 +17,10 @@ class Notification(models.Model):
         
     
 class EventStatusChange(Notification):
-    source_event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    source_event = models.ForeignKey('events.Event', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipients")
     
 
 class FriendRequest(Notification):
-    originator = models.ForeignKey(User, on_delete=models.CASCADE)
+    originator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="originator")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
