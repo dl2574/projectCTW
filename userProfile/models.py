bdf09e32334a0fa4fb387791a5305a5e11d93dab
dl2574 <20144415+dl2574@users.noreply.github.com>
2024-12-27 12:47:43 -0700

    Adjust Custom user model to add first name and last name to required fields

--- userProfile/models.py
+++ userProfile/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser):
     
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
