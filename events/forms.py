1cc026aa004be12bd56453f098729616a2391bd7
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-01-02 06:54:26 -0700

    Started comment view function, Found bug: Edit view creats new event instead of updating the selected event.

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Event
+from .models import Event, Comment
 
 
 class EventForm(ModelForm):
     class Meta():
         model = Event
         fields = ['name', 'description', 'location']
+
+class CommentForm(ModelForm):
+    class Meta():
+        model = Comment
+        fields = ['comment']
--- events/views.py
+++ events/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView, ListView
 
-from .forms import EventForm
-from .models import Event
+from .forms import EventForm, CommentForm
+from .models import Event, Comment
 from django.contrib.auth.decorators import login_required
 
 
@@ -36,9 +36,9 @@ def createEvent(request):
 
 @login_required(login_url="account_login")
 def editEvent(request, pk):
-    event = Event.objects.get(id=pk)
+    event = get_object_or_404(Event, id=pk)
     if request.user != event.created_by:
-        return redirect("home")
+        return redirect(event)
 
     form = EventForm(instance=event)
 
@@ -46,10 +46,11 @@ def editEvent(request, pk):
         form = EventForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("home")
+            return redirect()
 
     context = {"form": form}
     return render(request, "events/create_event.html", context)
+
 
 class EventDetailView(DetailView):
     model = Event
@@ -76,3 +77,9 @@ def upvoteEvent(request, pk):
     
     responseString = f"<html><i class='{thumb} fa-thumbs-up'></i> {num_of_votes} Up {vote_text}<html>"
     return HttpResponse(responseString)
+
+
+@login_required(login_url="account_login")
+def createComment(request):
+    form = CommentForm()
+
