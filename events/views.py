e358fbf0e892be74891b86b3bf8ea4baf69cf8c7
dl2574 <20144415+dl2574@users.noreply.github.com>
2026-02-20 20:41:27 -0700

    Minor template and syntax updates

--- events/views.py
+++ events/views.py
@@ -9,12 +9,12 @@ from .models import Event, Comment
 from django.contrib.auth.decorators import login_required
 
 
-
 class ProposedEvents(ListView):
     model = Event
     template_name = "events/proposed_events.html"
     context_object_name = "events"
-    
+
+
 proposedEvents = ProposedEvents.as_view()
 
 
@@ -40,12 +40,13 @@ def editEvent(request, pk):
 
     # Check if the logged-in user is the creator of the event
     if request.user != event.created_by:
-        return redirect(event) # Redirect to the event detail page (or some other page)
+        # Redirect to the event detail page (or some other page)
+        return redirect(event)
 
     if request.method == "POST":
         form = EventForm(request.POST, instance=event)
         if form.is_valid():
-            form.save() # Update the event object in the database
+            form.save()  # Update the event object in the database
             # Redirect to the event's detail page after saving
             return redirect("eventDetail", pk=event.id)
     else:
@@ -53,7 +54,6 @@ def editEvent(request, pk):
 
     context = {"form": form, "method": "edit", "pk": pk}
     return render(request, "events/event_form.html", context)
-
 
 
 def detailView(request, pk):
@@ -80,7 +80,8 @@ def detailView(request, pk):
         else:
             return redirect("account_login")
 
-    context = {"comments": event_comments, "commentForm": CommentForm, "event": event}
+    context = {"comments": event_comments,
+               "commentForm": CommentForm, "event": event}
     return render(request, "events/event_detail.html", context)
 
 
@@ -101,14 +102,14 @@ def upvoteEvent(request, pk):
 
     num_of_votes = event.number_of_upvotes()
 
-    # Check if this the vote count is above the required number of upvotes. If so, change status to planning.
+    # Check if this the vote count is above the required number of upvotes.
+    # If so, change status to planning.
     if num_of_votes > event.required_num_upvotes:
         event.status = Event.StatusCode.PLANNING
         event.save()
 
     vote_text = "Vote" if num_of_votes == 1 else "Votes"
-    
-    responseString = f"<html><i class='{thumb} fa-thumbs-up'></i> {num_of_votes} Up {vote_text}<html>"
+
+    responseString = f"<html><i class='{
+        thumb} fa-thumbs-up'></i> {num_of_votes} Up {vote_text}<html>"
     return HttpResponse(responseString)
-
-
