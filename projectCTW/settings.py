59466cbe0bdeb3e755ef5e206bf508487075e5b4
dl2574 <20144415+dl2574@users.noreply.github.com>
2026-02-22 14:12:06 -0700

    Update email backend service vars in settings.py

--- projectCTW/settings.py
+++ projectCTW/settings.py
@@ -226,10 +226,10 @@ if DEBUG:
 else:
     # Production: use SMTP (configure these in .env)
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    # EMAIL_HOST = env.str("EMAIL_HOST")
-    # EMAIL_HOST_PASSWORD = env.str("EMAIL_API_KEY")
-    # EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-    # EMAIL_USE_TLS = True
-    # EMAIL_PORT = env.int("EMAIL_PORT")
-    # EMAIL_SUBJECT_PREFIX = ''
-    # DEFAULT_FROM_EMAIL = env.str("FROM_EMAIL")
+    EMAIL_HOST = env.str("EMAIL_HOST")
+    EMAIL_HOST_PASSWORD = env.str("EMAIL_API_KEY")
+    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = env.int("EMAIL_PORT")
+    EMAIL_SUBJECT_PREFIX = ''
+    DEFAULT_FROM_EMAIL = env.str("FROM_EMAIL")
